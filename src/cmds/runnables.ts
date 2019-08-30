d785adebc34468725bb0743dfdc89b99b03f6b4e
Heyward Fann <fannheyward@gmail.com>
2019-08-30 22:52:50 +0800

    feat(cmds): quickpick runnables

--- src/cmds/runnables.ts
+++ src/cmds/runnables.ts
@@ -1,5 +1,5 @@
-import { TaskOptions, workspace } from 'coc.nvim';
-import { Position, TextDocumentIdentifier } from 'vscode-languageserver-protocol';
+import { Terminal, TerminalOptions, workspace } from 'coc.nvim';
+import { Position, Range, TextDocumentIdentifier } from 'vscode-languageserver-protocol';
 import { Server } from '../server';
 
 // import { CargoWatchProvider, registerCargoWatchProvider } from './cargo_watch';
@@ -15,35 +15,51 @@ interface Runnable {
   args: string[];
   env: { [index: string]: string };
   cwd?: string;
+  range: Range;
 }
 
-let prevTask: any;
+class RunnableQuickPick {
+  label: string;
+
+  constructor(public runnable: Runnable) {
+    this.label = runnable.label;
+  }
+}
+
 export async function handle() {
   const { document, position } = await workspace.getCurrentState();
   if (document.languageId !== 'rust') {
     return;
   }
 
+  workspace.showMessage(`Fetching runnable...`);
+
   const params: RunnablesParams = {
     textDocument: { uri: document.uri },
     position
   };
   const runnables = await Server.client.sendRequest<Runnable[]>('rust-analyzer/runnables', params);
-  for (const r of runnables) {
-    if (prevTask && prevTask.running) {
-      continue;
-    }
 
-    const task = workspace.createTask('Rust');
-    const options: TaskOptions = {
-      cmd: r.bin,
-      args: r.args,
-      cwd: r.cwd
-      // r.env
-    };
-    prevTask = task;
-    await task.start(options);
+  const items: RunnableQuickPick[] = [];
+  for (const r of runnables) {
+    items.push(new RunnableQuickPick(r));
   }
+
+  const idx = await workspace.showQuickpick(items.map(o => o.label));
+  if (idx === -1) {
+    return;
+  }
+
+  const runnable = items[idx].runnable;
+  const cmd = `${runnable.bin} ${runnable.args.join(' ')}`;
+  const opt: TerminalOptions = {
+    name: runnable.label,
+    cwd: runnable.cwd,
+    env: runnable.env
+  };
+  workspace.createTerminal(opt).then((t: Terminal) => {
+    t.sendText(cmd);
+  });
 }
 
 // export async function handleSingle(runnable: Runnable) {
