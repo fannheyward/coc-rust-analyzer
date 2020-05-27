29d48111c44822ab4ced6a8ae1a18335de05d0b5
Heyward Fann <fannheyward@gmail.com>
2020-05-27 11:39:55 +0800

    chore(downloader): patcheif for NixOS
    closes #260

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,5 +1,6 @@
+import { exec } from 'child_process';
 import { ExtensionContext, workspace } from 'coc.nvim';
-import * as fs from 'fs';
+import { createWriteStream, PathLike, promises as fs } from 'fs';
 import fetch from 'node-fetch';
 import os from 'os';
 import { join } from 'path';
@@ -8,6 +9,38 @@ import util from 'util';
 import { UpdatesChannel } from './config';
 
 const pipeline = util.promisify(stream.pipeline);
+
+async function patchelf(dest: PathLike): Promise<void> {
+  const expression = `
+{src, pkgs ? import <nixpkgs> {}}:
+    pkgs.stdenv.mkDerivation {
+        name = "rust-analyzer";
+        inherit src;
+        phases = [ "installPhase" "fixupPhase" ];
+        installPhase = "cp $src $out";
+        fixupPhase = ''
+        chmod 755 $out
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out
+        '';
+    }
+`;
+  const origFile = dest + '-orig';
+  await fs.rename(dest, origFile);
+
+  await new Promise((resolve, reject) => {
+    const handle = exec(`nix-build -E - --arg src '${origFile}' -o ${dest}`, (err, stdout, stderr) => {
+      if (err != null) {
+        reject(Error(stderr));
+      } else {
+        resolve(stdout);
+      }
+    });
+    handle.stdin?.write(expression);
+    handle.stdin?.end();
+  });
+
+  await fs.unlink(origFile);
+}
 
 export interface ReleaseTag {
   tag: string;
@@ -67,9 +100,17 @@ export async function downloadServer(context: ExtensionContext, updatesChannel: 
     statusItem.text = `${p}% Downloading rust-analyzer ${latest.tag}`;
   });
 
-  const destFileStream = fs.createWriteStream(_path, { mode: 0o755 });
+  const destFileStream = createWriteStream(_path, { mode: 0o755 });
   await pipeline(resp.body, destFileStream);
 
   await context.globalState.update('release', latest.tag);
+
+  try {
+    if (await fs.stat('/etc/nixos')) {
+      statusItem.text = `Patching rust-analyzer executable...`;
+      await patchelf(_path);
+    }
+  } catch (e) {}
+
   statusItem.hide();
 }
