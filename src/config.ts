35caf3e67bf9898a79a5f00a94171a85c82063d7
Heyward Fann <fannheyward@gmail.com>
2020-09-30 09:41:41 +0800

    chore: rust-analyzer.lens.methodReferences config

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export type UpdatesChannel = 'stable' | 'nightly';
 
 export class Config {
   private static readonly rootSection = 'rust-analyzer';
-  private static readonly requiresReloadOpts = ['serverPath', 'cargo', 'procMacro', 'files', 'updates', 'lens'].map((opt) => `${Config.rootSection}.${opt}`);
+  private static readonly requiresReloadOpts = ['serverPath', 'cargo', 'procMacro', 'files', 'updates', 'lens', 'hoverActions'].map((opt) => `${Config.rootSection}.${opt}`);
   private cfg: WorkspaceConfiguration;
 
   constructor() {
