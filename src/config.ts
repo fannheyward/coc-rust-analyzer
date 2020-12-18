ae3972ba036476872a014ae6ce8d111def65ebd2
Heyward Fann <fannheyward@gmail.com>
2020-12-18 16:48:30 +0800

    fix: inlayHints.enable

--- src/config.ts
+++ src/config.ts
@@ -32,7 +32,7 @@ export class Config {
   get inlayHints() {
     const hasVirtualText = workspace.isNvim && workspace.nvim.hasFunction('nvim_buf_set_virtual_text');
     return {
-      enable: hasVirtualText && this.cfg.get<boolean>('enable'),
+      enable: hasVirtualText && this.cfg.get<boolean>('inlayHints.enable'),
       typeHints: hasVirtualText && this.cfg.get<boolean>('inlayHints.typeHints'),
       typeHintsSeparator: this.cfg.get<string>('inlayHints.typeHintsSeparator'),
       chainingHints: hasVirtualText && this.cfg.get<boolean>('inlayHints.chainingHints'),
